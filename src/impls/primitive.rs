0fde9d692bc0e1586b216f59be2acad5ea887f2c
melvdl <hansw533@gmail.com>
2024-04-08 21:55:49 +0200

    initial commit

---
+++ src/impls/primitive.rs
@@ -0,0 +1,126 @@
+use crate::{Deserialize, DeserializeIntoUninit, SerialSize, Serialize};
+use core::char::CharTryFromError;
+use core::mem::{size_of, MaybeUninit};
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
+pub struct IllegalBitPattern;
+
+macro_rules! to_e_bytes {
+    ($expr:expr) => {{
+        #[cfg(feature = "ne")]
+        {
+            ($expr).to_ne_bytes()
+        }
+        #[cfg(feature = "le")]
+        {
+            ($expr).to_ne_bytes()
+        }
+        #[cfg(feature = "be")]
+        {
+            ($expr).to_be_bytes()
+        }
+    }};
+}
+
+macro_rules! from_e_bytes {
+    ($ty:ty, $expr:expr) => {{
+        #[cfg(feature = "ne")]
+        {
+            <$ty>::from_ne_bytes($expr)
+        }
+        #[cfg(feature = "le")]
+        {
+            <$ty>::from_le_bytes($expr)
+        }
+        #[cfg(feature = "be")]
+        {
+            <$ty>::from_be_bytes($expr)
+        }
+    }};
+}
+
+macro_rules! impl_primitive {
+    ($ty:ty) => {
+        impl crate::SerialSize for $ty {
+            const SIZE: usize = core::mem::size_of::<$ty>();
+        }
+
+        impl crate::Serialize for $ty {
+            fn serialize(&self, buffer: &mut [u8; <Self as crate::SerialSize>::SIZE]) {
+                *buffer = to_e_bytes!(self);
+            }
+        }
+
+        impl crate::DeserializeIntoUninit for $ty {
+            type Error = core::convert::Infallible;
+
+            fn deserialize_into_uninit<'a>(
+                into: &'a mut MaybeUninit<Self>,
+                buffer: &[u8; <Self as crate::SerialSize>::SIZE],
+            ) -> Result<&'a mut Self, <Self as crate::Deserialize>::Error> {
+                Ok(into.write(from_e_bytes!($ty, *buffer)))
+            }
+        }
+    };
+}
+
+impl_primitive!(u8);
+impl_primitive!(u16);
+impl_primitive!(u32);
+impl_primitive!(u64);
+impl_primitive!(u128);
+impl_primitive!(usize);
+impl_primitive!(i8);
+impl_primitive!(i16);
+impl_primitive!(i32);
+impl_primitive!(i64);
+impl_primitive!(i128);
+impl_primitive!(isize);
+impl_primitive!(f32);
+impl_primitive!(f64);
+
+impl SerialSize for bool {
+    const SIZE: usize = <u8 as SerialSize>::SIZE;
+}
+
+impl Serialize for bool {
+    fn serialize(&self, buffer: &mut [u8; <Self as SerialSize>::SIZE]) {
+        *buffer = [*self as u8]
+    }
+}
+
+impl DeserializeIntoUninit for bool {
+    type Error = IllegalBitPattern;
+
+    fn deserialize_into_uninit<'a>(
+        into: &'a mut MaybeUninit<Self>,
+        buffer: &[u8; <Self as SerialSize>::SIZE],
+    ) -> Result<&'a mut Self, Self::Error> {
+        Ok(into.write(match *buffer {
+            [0] => false,
+            [1] => true,
+            _ => Err(IllegalBitPattern)?,
+        }))
+    }
+}
+
+impl SerialSize for char {
+    const SIZE: usize = size_of::<u32>();
+}
+
+impl Serialize for char {
+    fn serialize(&self, buffer: &mut [u8; <Self as SerialSize>::SIZE]) {
+        u32::serialize(&u32::from(*self), buffer)
+    }
+}
+
+impl DeserializeIntoUninit for char {
+    type Error = CharTryFromError;
+
+    fn deserialize_into_uninit<'a>(
+        into: &'a mut MaybeUninit<Self>,
+        buffer: &[u8; <Self as SerialSize>::SIZE],
+    ) -> Result<&'a mut Self, Self::Error> {
+        Ok(into.write(char::try_from(u32::deserialize(buffer).unwrap())?))
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+//! AAAA
+//!
+//! HELP I'M STUCK IN A CRATE DOC QUICK CARGO IS COMING
+
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+#![feature(array_chunks)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use core::mem::MaybeUninit;
+#[cfg(any(
+    all(feature = "ne", any(feature = "le", feature = "be")),
+    all(feature = "le", any(feature = "ne", feature = "be")),
+    all(feature = "be", any(feature = "ne", feature = "le")),
+))]
+compile_error!(r#"features "ne", "le" and "be" cannot be used together"#);
+
+pub mod impls;
+
+/// A trait indicating the size of the serialized form of `Self`.
+pub trait SerialSize {
+    /// The size of the buffer to serialize into.
+    const SIZE: usize;
+}
+
+/// Serialize `Self` into a const-sized byte buffer.
+pub trait Serialize: SerialSize {
+    /// Serialize `self` into the provided `buffer`.
+    fn serialize(&self, buffer: &mut [u8; Self::SIZE]);
+}
+
+/// Deserialize `Self` from a const-sized byte buffer.
+pub trait Deserialize: SerialSize + Sized {
+    /// The error type that can occur during deserialization.
+    type Error;
+
+    /// Deserialize a `Self` from the provided `buffer`.
+    fn deserialize(buffer: &[u8; Self::SIZE]) -> Result<Self, Self::Error>;
+}
+
+/// Deserialize `Self` from a const-sized byte buffer directly into a pre-allocated [`MaybeUninit<Self>`].
+pub trait DeserializeIntoUninit: SerialSize + Sized {
+    /// The error type that can occur during deserialization.
+    type Error;
+
+    /// Deserialize a `Self` from the provided `buffer`.
+    /// Returns a mutable refernce to the initialized contents of `into`.
+    ///
+    /// The usual rules of [`MaybeUninit::write`] apply.
+    fn deserialize_into_uninit<'a>(
+        into: &'a mut MaybeUninit<Self>,
+        buffer: &[u8; Self::SIZE],
+    ) -> Result<&'a mut Self, Self::Error>;
+}
+
+impl<T> Deserialize for T
+where
+    T: DeserializeIntoUninit,
+{
+    type Error = <Self as DeserializeIntoUninit>::Error;
+
+    fn deserialize(buffer: &[u8; Self::SIZE]) -> Result<Self, Self::Error> {
+        let mut result = MaybeUninit::uninit();
+        <Self as DeserializeIntoUninit>::deserialize_into_uninit(&mut result, buffer)?;
+        // Safety: we just initialized this
+        Ok(unsafe { result.assume_init() })
+    }
+}
